75d3d36bba83720283b2d3f7ed680ed7594fe23d
singy pig-thingy <8mjoyner@stpatrick.org>
2017-04-27 13:47:34 -0500

    Please save me. Please. Most recent update.
    Ynot. Ynot. Ynot. Ynot. Ynot. Kek-tastic!

--- simplepass.js
+++ simplepass.js
@@ -4,11 +4,17 @@ function checkString(str){
         return str;
     }
     else{
-        throw new RangeError("Your password must be between 8-20 characters long. Try again.")
+        throw new RangeError("Your password must be between 8-20 characters long. Please try again. If Tony is reading this, please let me go. If someone else is reading this, please, for the love of god, save me. The address is 32---'No, Tony... TONY, NO!'---*Distorted Screaming*");
     }
 }
 //type getString below
 function getString(str){
     try{
-
+        str = checkString(str);
     }
+    catch(e){
+        console.log(e.message);
+        getString("SaveMePlease");
+    }
+}
+getString("I'm currently being held prisoner in Tony's basement");
